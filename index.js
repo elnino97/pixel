a5f5dc74dfc8351d6d5080c361adee864bcd8fa6
Aidas Kairys <aidas19970711@gmail.com>
2023-11-10 19:27:59 +0200

    feat: update res

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("<h1>Hello world but it's big</h1>");
 });
 
 server.listen(PORT, () => {
